325b09b84b2003bb54840f62fe14f9c7bf7c039e
Viktor Morovič <257026@vutbr.cz>
2024-04-12 21:21:12 +0200

    first commit

--- searching.py
+++ searching.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -11,11 +11,20 @@ def read_data(file_name, field):
     :param field: (str), field of a dict to return
     :return: (list, string),
     """
-    file_path = os.path.join(cwd_path, file_name)
-
+    file_path = os.path.join(cwd_path, "lecture_searching", file_name)
+    with open(file_path, mode="r") as file:
+        data = json.load(file)
+        for key, value in data.items():
+            if key == field:
+                return value
+            else:
+                continue
+        return None
 
 def main():
-    pass
+    sequential_data = read_data(file_name="sequential.json", field="unordered_numbers")
+    print(sequential_data)
+    
 
 
 if __name__ == '__main__':
